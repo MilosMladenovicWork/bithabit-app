fbe938e9220e992bb03ab17ca65c86ca4723ea04
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-02-27 18:11:53 +0100

    added Home page

--- src/components/AddHabitForm.js
+++ src/components/AddHabitForm.js
@@ -1,23 +1,23 @@
 import React, {useState} from 'react'
 import './Form.css'
 
+function AddHabitForm(props){
 
-function SignUpForm(props){
-  
   const form = useState({})
 
 
   const changeHandler = (e) => {
     form[e.target.name] = e.target.value
   }
+
   return(
     <form onSubmit={(e) => e.preventDefault()} className='form' method='POST' action={props.action || "#"}>
       <input type='text' name='username'  placeholder='Username' value={props.username} onChange={changeHandler}/>
       <input type='email' name='email'  placeholder='Email' value={props.email} onChange={changeHandler}/>
       <input type='password' name='password'  placeholder='Password' value={props.password} onChange={changeHandler}/>
-      <button type='submit' style={{backgroundColor:"#F54BEA"}}>Sign Up</button>
+      <button type='submit'>Add</button>
     </form>
   )
 }
 
-export default SignUpForm
+export default AddHabitForm
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import DragPage from '../components/DragPage'
+import AddHabitForm from '../components/AddHabitForm'
+import './HomePage.css'
 
 function HomePage(props){
+
+  let habits = [
+    {
+      icon:'brackets',
+      habit:'Refactor Code',
+      description:'Clean code for 15 minutes a day',
+      done:'false'
+    },
+    {
+      icon:'brackets',
+      habit:'Refactor Code',
+      description:'Clean code for 15 minutes a day',
+      done:'true'
+    }
+  ]
+
+  const habitsArr = habits.map((habit, index) => {
+    return <div key={index} className='habit' style={{opacity:habit.done === 'true' ? 0.4 : 1}}>
+      <p>{habit.icon}</p>
+      <p>{habit.habit}</p>
+      <p>{habit.done}</p>
+    </div>
+  })
+
   return(
-    <div className='HomePage'>
+    <div className='home-page'>
+      <h1 style={{color:"white"}}>Welcome, User!</h1>
+      {habitsArr}
       <DragPage 
         style={{
-          top:"80vh",
+          top:"82.5vh",
+          borderRadius:"10px"
         }}
       >
-        Hey
+        <p>plus button</p>
+        <AddHabitForm/>
       </DragPage>
     </div>
   )
