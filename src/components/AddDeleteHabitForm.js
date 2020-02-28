10c3dd53d28428afa3d7322bd6c7505fbc5e47d6
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-02-28 17:08:37 +0100

    added swiper for the icons

--- src/components/AddDeleteHabitForm.js
+++ src/components/AddDeleteHabitForm.js
@@ -1,9 +1,19 @@
 import React, {useState} from 'react'
+import Swiper from 'react-id-swiper'
+import 'swiper/css/swiper.css'
 import './Form.css'
+import sampleImg from '../media/sample.svg'
 
 function AddDeleteHabitForm(props){
 
   const form = useState({})
+
+  const swiperParams = {
+    slidesPerView:3,
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true
+  }
 
 
   const changeHandler = (e) => {
@@ -12,6 +22,12 @@ function AddDeleteHabitForm(props){
 
   return(
     <form onSubmit={(e) => e.preventDefault()} className='form' method='POST' action={props.action || "#"}>
+      <Swiper {...swiperParams}>
+        <img src={sampleImg}/>
+        <img src={sampleImg}/>
+        <img src={sampleImg}/>
+        <img src={sampleImg}/>
+      </Swiper>
       <input type='text' name='habit'  placeholder='Habit' value={form.habit} onChange={changeHandler}/>
       <textarea name='description' value={form.description} onChange={changeHandler} placeholder='Description'></textarea>
       <button type='submit'>Add</button>
